3e3b75fd4d8d159dcf211661f8ae98ffc1a49d90
Paul Ticona <paulticona264riddick@gmail.com>
2022-08-11 11:44:25 -0500

    thema par ale proyecto

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#171A21",
+            contrastText: "#ffffff",
+        }
+    }
+});
+
+export default theme;
